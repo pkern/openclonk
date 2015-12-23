66dc50b8eab1fab14ec2a8f92fddb98239ca5e22
Günther Brammer <gbrammer@gmx.de>
2015-12-23 23:10:48 +0100

    Script: Add Scenario effects
    These work just like global effects, except that effects from CreateEffect
    get the scenario as target parameter in the callbacks.

--- src/game/C4GameScript.cpp
+++ src/game/C4GameScript.cpp
@@ -49,6 +49,8 @@ C4Effect ** FnGetEffectsFor(C4PropList * pTarget)
 	{
 		if (pTarget == ScriptEngine.GetPropList())
 			return &ScriptEngine.pGlobalEffects;
+		if (pTarget == GameScript.GetPropList())
+			return &GameScript.pScenarioEffects;
 		C4Object * Obj = pTarget->GetObject();
 		if (!Obj)
 			throw C4AulExecError("Effect target has to be an object");
